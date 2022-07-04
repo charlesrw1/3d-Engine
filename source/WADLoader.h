209b262fbcf1842e96f3c8fa269120dec6dc8f22
Charles Weiler <charlierweiler@gmail.com>
2022-07-03 19:32:49 -0700

    Removed unused files

--- source/WADLoader.h
+++
@@ -1,8 +0,0 @@
-#ifndef WADLOADER_H
-#define WADLOADER_H
-
-// Loads a .wad file (the texture file for Quake + Half-Life) and uploads the data to the GPU
-// Textures are stored in the texture manager under (texture_name).wad
-bool load_wad_file(const char* wad_name);
-
-#endif // !WADLOADER_H
